fec94ecfed75b16d7b3b8410c883347baf1140bf
sebastian-meier <contact@sebastianmeier.eu>
2015-09-25 16:20:39 +0100

    Initial Commit

--- README.md
+++ README.md
@@ -1,2 +1,42 @@
 # nmap-squared.js
 Extension to the nmap algorithm to derive at an equal-sized treemap within a rectangle
+
+![Examples](https://raw.githubusercontent.com/sebastian-meier/nmap-squared.js/master/img/nmap-squared.png)
+
+## Method
+
+The function is a helper for equally sized treemap generation, using the nmap algorithm. The approach adds new empty points to the set to achieve a square number of points that can be then visualized with the same are.
+
+Two examples with US states and London Boroughs, can be found [here](http://prjcts.sebastianmeier.eu/nmap/squared/examples/index.html)
+
+The function incorporates two approaches, the first approach "border" is placing new points at the outer extent of the dataset and the second approach "quad" is using a quadtree function to find empty areas to place new points. A visual comparison can be found [here](http://prjcts.sebastianmeier.eu/nmap/squared/examples/process.html).
+
+## Usage
+
+You can use it in combination with the original [nmap.js library](https://github.com/sebastian-meier/nmap.js).
+
+```
+var border = nmap_squared({
+	//Max-Extent of the output data
+	width:300,
+	height:300,
+	//Array with points {x:,y:}
+	data:data,
+	//Approach to use either border or quad
+	method:"border"
+});
+
+var border_elements = [];
+for(var i = 0; i<border.data.length; i++){
+	border_elements.push(new nmap_element({
+		id:i,
+		x:parseFloat(border.data[i].x),
+		y:parseFloat(border.data[i].y),
+		weight:10000+Math.random(),
+		klass:border.data[i].class
+	}));
+}
+var border_map = new nmap({x:0,y:0,width:parseFloat(border.width), height:parseFloat(border.height)});
+var border_ac = border_map.alternateCut({elements:border_elements});
+```
+For further information on how to use the nmap features check the [nmap repo](https://github.com/sebastian-meier/nmap.js).
---
+++ nmap-squared.js
@@ -0,0 +1,362 @@
+/*global nmap,d3,nmap_extend */
+/*jshint unused:false*/
+
+/**
+* helper algorithm for the nmap.js algorithm
+* @constructor
+* @param {object} args
+* @param {array} args.data - point data
+* @param {flaot|integer} args.data[].x - x position
+* @param {flaot|integer} args.data[].y - y position
+* @param {integer|float} args.height=1 - height of the available space
+* @param {integer|float} args.width=1 - width of the available space
+* @param {string} args.method="border||quad" -  Filling method either border or quad(tree) approach
+* @return {object} nmap_squared
+*/
+
+var nmap_squared = function(args){
+	if(typeof(nmap) !== typeof(Function)){ throw "ERROR: You need to include the nmap.js library."; }
+
+	var defaults = {
+		data:[],
+		height:1,
+		width:1,
+		method:"border"
+	};
+
+	//given arguments and defaults
+	var attr = nmap_extend(defaults, args);
+
+	//result object
+	var result = [], swidth, sheight, sx, sy;
+
+	/**
+	* Create squared
+	* @constructor
+	*/
+	function squared(){
+		var i;
+
+		/*--- Calculate the extend of the data and normalize it ---*/
+		var maxX = -Number.MAX_VALUE,
+			minX = Number.MAX_VALUE,
+			maxY = -Number.MAX_VALUE,
+			minY = Number.MAX_VALUE;
+
+		var x,y;
+
+		for(i = 0; i<attr.data.length; i++){
+			x = parseFloat(attr.data[i].x);
+			y = parseFloat(attr.data[i].y);
+
+			if(x > maxX){maxX = x;}
+			if(x < minX){minX = x;}
+
+			if(y > maxY){maxY = y;}
+			if(y < minY){minY = y;}
+		}
+
+		swidth = maxX - minX;
+		sheight = maxY - minY;
+		sx = attr.width/swidth;
+		sy = attr.height/sheight;
+
+		if(sx<sy){
+			sy = sx;
+		}else{
+			sx = sy;
+		}
+
+		//Create the result array with new positions which are normalized to the available space
+		for(i = 0; i<attr.data.length; i++){
+			result.push({
+				x:(parseFloat(attr.data[i].x)-minX)*sx,
+				y:(parseFloat(attr.data[i].y)-minY)*sy,
+				//For the nmap algorithm this highlights an original position
+				//0 indicates empty cells added by this algorithm
+				class:1
+			});
+		}
+
+		//How many empty cells do we need to add to the set?
+		var grid_size = Math.ceil(Math.sqrt(result.length)),
+			sq_amount = Math.pow(grid_size,2),
+			sq_missing = sq_amount-result.length;
+
+		//Decide upon which method to use for placing the empty cells?
+		switch(attr.method){
+			case 'border':
+				/*--- Add empty cells from the outer border of the extent of the dataset ---*/
+				
+				//We will create an array with possible new points which are equally distributed along the extent-rectangle
+				var extra_points = [];
+
+				//Top and bottom border
+				for(x = 0; x<grid_size; x++){
+					extra_points.push({class:0,dist:Number.MAX_VALUE,x:((swidth*sx)/grid_size*x+((swidth*sx)/grid_size/2)),y:0});
+					extra_points.push({class:0,dist:Number.MAX_VALUE,x:((swidth*sx)/grid_size*x+((swidth*sx)/grid_size/2)),y:(sheight*sy)});
+				}
+
+				//Left and right border
+				for(y = 0; y<grid_size; y++){
+					extra_points.push({class:0,dist:Number.MAX_VALUE,x:0, y:((sheight*sy)/grid_size*y+((sheight*sy)/grid_size/2))});
+					extra_points.push({class:0,dist:Number.MAX_VALUE,x:(swidth*sx),y:((sheight*sy)/grid_size*y+((sheight*sy)/grid_size/2))});
+				}
+
+				//Adding new points until the rectangle is full
+				while(sq_missing>0){
+					//Choosing the point which is the furthest away from its neighbours 
+					extra_points = squared.calcDist(extra_points, result);
+					result.push(extra_points[extra_points.length-1]);
+					sq_missing--;					
+				}
+			break;
+			case 'quad':
+				/*--- Add empty cells by calculating empty areas with a quadtree function ---*/
+				if(typeof(d3.geom.quadtree) !== typeof(Function)){ throw "ERROR: If you want the quadtree method need to include the d3 library."; }
+
+				//Prepare the data to be passed to d3's quadtree function
+				var quad_data = [];
+				for(i = 0; i<result.length; i++){
+					quad_data.push([result[i].x, result[i].y]);
+				}
+
+				//Initiate d3's quadtree
+				var quad_extent = [[0,0],[(swidth*sx), (sheight*sy)]];
+				var quadtree = d3.geom.quadtree()
+					.extent(quad_extent)
+					(quad_data);
+
+				var qwidth = swidth*sx;
+				var qheight = sheight*sy;
+				if(qwidth < qheight){qwidth = qheight;}{qheight = qwidth;}
+				
+				//Finding the empty squares
+				var emptySquares = squared.checkForEmptySquares(quadtree, 0, qwidth, qheight, 0, 0);
+
+				//Measuring the size of the empty squares
+				emptySquares = squared.measureSquares(emptySquares, swidth*sx, sheight*sy);
+
+				//Add empty squares
+				while(sq_missing>0){
+					result.push(emptySquares[(emptySquares.length-sq_missing)]);
+					sq_missing--;					
+				}
+			break;
+		}
+	}
+
+	/**
+	* calculate distance between points in two arrays
+	* @param {array} array1
+	* @param {array} array1.x - x position
+	* @param {array} array1.y - y position
+	* @param {array} array1.dist - max distance to array2 points
+	* @param {array} array2
+	* @param {array} array2.x - x position
+	* @param {array} array2.y - y position
+	* @return {array} array1 - array1 with updated dist attribute
+	*/
+	squared.calcDist = function(array1, array2){
+		for(var i = 0; i<array1.length; i++){
+			for(var ii = 0; ii<array2.length; ii++){
+				var t_dist = Math.sqrt((array1[i].x-array2[ii].x)*(array1[i].x-array2[ii].x)+(array1[i].y-array2[ii].y)*(array1[i].y-array2[ii].y));
+				if(t_dist < array1[i].dist){
+					array1[i].dist = t_dist;
+				}
+			}
+		}
+
+		array1.sort(function(a1,a2){
+			if(a1.dist < a2.dist){
+				return -1;
+			}else if(a1.dist > a2.dist){
+				return 1;
+			}else{
+				return 0;
+			}
+		});
+
+		return array1;
+	};
+
+	/**
+	* measure area of rectangles, rectangles that go beyond width/height are resized
+	* rectangles outside width/height are ignored (size = 0)
+	* @param {array} squares
+	* @param {float|integer} squares.x - x of upper left corner of the rectangle
+	* @param {float|integer} squares.y - y of upper left corner of the rectangle
+	* @param {float|integer} squares.width - height of the rectangle
+	* @param {float|integer} squares.height - height of the rectangle
+	* @param {float|integer} squares.size - area of the rectangle
+	* @param {float|integer} width - extent width
+	* @param {float|integer} height - extent height
+	* @return {array} squares - updated array of rectangles with size
+	*/
+	squared.measureSquares = function(squares, width, height){
+		for(var i = 0; i<squares.length; i++){
+			squares[i].swidth = squares[i].width;
+			squares[i].sheight = squares[i].height;
+
+			if((squares[i].x > width)||(squares[i].y > height)){
+				squares[i].size = 0;
+			}else{
+				if((squares[i].x+squares[i].width) > width){
+					squares[i].swidth = width-squares[i].x;
+				}
+				if((squares[i].y+squares[i].height) > height){
+					squares[i].sheight = height-squares[i].y;
+				}
+				squares[i].size = squares[i].sheight*squares[i].swidth;
+			}
+		}
+
+		squares.sort(function(a1, a2){
+			if(a1.size < a2.size){
+				return -1;
+			}else if(a1.size > a2.size){
+				return 1;
+			}else{
+				return 0;
+			}
+		});
+
+		return squares;
+	};
+
+	/**
+	* find empty rectangles within a quadtree map
+	* rectangles outside width/height are ignored (size = 0)
+	* @param {array} node - d3 quadtree node object (see d3 for further info)
+	* @param {integer} level - treemap level
+	* @param {float|integer} width - width of current quadtree area
+	* @param {float|integer} height - height of current quadtree area
+	* @param {float|integer} x - upper left x of current quadtree area
+	* @param {float|integer} y - upper left y of current quadtree area
+	* @return {array} t_emptySquares - array of empty squares in the treemap
+	*/
+	squared.checkForEmptySquares = function(node, level, width, height, x, y){
+		var t_emptySquares = [], x_dist, y_dist, i;
+		//Don't do it on nodes
+		//TODO: sometimes it happens that one leaf occupies a large area, in the future we need to calculate the unoccupied area within a leaf for better performance
+		if(!node.leaf){
+			var real_length = 0;
+			for(i in node.nodes){
+				real_length++;
+			}
+			if(real_length<4){
+				//Luckily the keys of the child-nodes are still in the right order
+				//0 - top left
+				//1 - top reight
+				//2 - bottom left
+				//3 - bottom right
+				//So we can figure out which ones are missing and calculate their extent
+				var missings = [];
+				for(i = 0; i<4; i++){
+					if(!(i in node.nodes)){
+						missings.push(i);
+					}
+				}
+				var missed = [];
+
+				//If two areas next to each other are missing > combine them to one large area
+				if((missings.indexOf(0)>=0)&&(missings.indexOf(1)>=0)){
+					missed.push(0);
+					missed.push(1);
+					t_emptySquares.push(squared.createMissingRect(2, 1, level, width, height, x, y));
+				}else if((missings.indexOf(2)>=0)&&(missings.indexOf(3)>=0)){
+					missed.push(2);
+					missed.push(3);
+					t_emptySquares.push(squared.createMissingRect(2, 1, level, width, height, x, y+(height/2)));
+				}else if((missings.indexOf(0)>=0)&&(missings.indexOf(2)>=0)){
+					missed.push(0);
+					missed.push(2);
+					t_emptySquares.push(squared.createMissingRect(1, 2, level, width, height, x, y));
+				}else if((missings.indexOf(1)>=0)&&(missings.indexOf(3)>=0)){
+					missed.push(1);
+					missed.push(3);
+					t_emptySquares.push(squared.createMissingRect(1, 2, level, width, height, x+(width/2), y));
+				}
+
+				//The single missing squares are just added
+				for(i = 0; i<4; i++){
+					if((missed.indexOf(parseInt(i))<0)&&(missings.indexOf(parseInt(i))>=0)){
+						x_dist=0;
+						y_dist=0;
+						switch(parseInt(i)){
+							case 0:
+								//nothing
+							break;
+							case 1:
+								x_dist =(width/2);
+							break;
+							case 2:
+								y_dist = (height/2);
+							break;
+							case 3:
+								x_dist = (width/2);
+								y_dist = (height/2);
+							break;
+						}
+						t_emptySquares.push(squared.createMissingRect(1, 1, level, width, height, x+x_dist, y+y_dist));	
+					}
+				}
+			}
+			//For the child-squares that have child nodes, do the checking again.
+			for(i = 0; i<4; i++){
+				if((i in node.nodes)){
+					x_dist=0;
+					y_dist=0;
+					switch(parseInt(i)){
+						case 0:
+							//nothing
+						break;
+						case 1:
+							x_dist = (width/2);
+						break;
+						case 2:
+							y_dist = (height/2);
+						break;
+						case 3:
+							x_dist = (width/2);
+							y_dist = (height/2);
+						break;
+					}
+					t_emptySquares = t_emptySquares.concat(squared.checkForEmptySquares(node.nodes[i], level+1, (width/2), (height/2), x+x_dist, y+y_dist));
+
+				}
+			}
+		}
+
+		return t_emptySquares;
+	};
+
+	/**
+	* create a rectangle object
+	* @param {float|integer} x_dist - over how many squares does the new rectangle go (1 or 2)
+	* @param {float|integer} y_dist - over how many squares does the new rectangle go (1 or 2)
+	* @param {float|integer} level - level within in the treemap (indicator of size)
+	* @param {float|integer} width - width of parent square
+	* @param {float|integer} height - height of parent square
+	* @param {float|integer} x - x of upper left corner of rectangle
+	* @param {float|integer} y - y of upper left corner of rectangle
+	* @return {array} t_emptySquares - array of empty squares in the treemap
+	*/
+	squared.createMissingRect = function(x_dist, y_dist, level, width, height, x, y){
+		return {
+			ox : x,
+			oy : y,
+			x: x + ((width/2)*x_dist)/2,
+			y: y + ((height/2)*y_dist)/2,
+			height : (height/2)*y_dist,
+			width : (width/2)*x_dist,
+			level : level,
+			size:0,
+			class:0
+		};
+	};
+
+	squared();
+
+	return {width:swidth*sx, height:sheight*sy, data:result};
+};
